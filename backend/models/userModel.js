c221a19d7a1f4fc55e8c8102422de6f50dd3b054
hp15aug <hpaneru04@gmail.com>
2024-10-04 23:40:06 +0530

    models created

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    isAdmin:{
+        type: Boolean,
+        required: true,
+        defualt: false
+    }
+}, {
+    timestamps: true,
+});
+
+const User= mongoose.model("User", userSchema);
+
+export default User;
