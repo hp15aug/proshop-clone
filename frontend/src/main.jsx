e0fa45ff2598f12eb7a1850434e9a789d760fb15
hp15aug <hpaneru04@gmail.com>
2024-10-12 16:34:07 +0530

    Shipping Screen added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,7 +16,8 @@ import HomeScreen from './screens/HomeScreen.jsx';
 import ProductScreen from './screens/ProductScreen.jsx';
 import CartScreen from './screens/CartScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
-import RegisterScreen from './screens/RegisterScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx';
+import ShippingScreen from './screens/ShippingScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={< LoginScreen />} />
       <Route path='/register' element={< RegisterScreen />} />
+      <Route path='/shipping' element={< ShippingScreen />} />
     </Route>
   )
 );
