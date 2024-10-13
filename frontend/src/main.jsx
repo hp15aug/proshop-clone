f9c30b503563b238a340ff62e426c4a1a7e8435f
hp15aug <hpaneru04@gmail.com>
2024-10-13 13:07:40 +0530

    PaymentScreen added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,12 +12,14 @@ import {
 import  store from "./store.js";
 import {Provider} from "react-redux";
 
+import PrivateRoute from './components/PrivateRoute.jsx'
 import HomeScreen from './screens/HomeScreen.jsx';
 import ProductScreen from './screens/ProductScreen.jsx';
 import CartScreen from './screens/CartScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import ShippingScreen from './screens/ShippingScreen.jsx';
+import PaymentScreen from './screens/PaymentScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +29,11 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={< LoginScreen />} />
       <Route path='/register' element={< RegisterScreen />} />
-      <Route path='/shipping' element={< ShippingScreen />} />
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={< ShippingScreen />} />  
+        <Route path='/payment' element={< PaymentScreen />} />  
+      </Route>
     </Route>
   )
 );
